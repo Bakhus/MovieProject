dc51b7423f1544a93dbcd359ae51d4a4c241596f
ds1c1 <apple9one@gmail.com>
2016-05-12 17:09:08 -0400

    Update column_genre_len.py

--- column_genre_len.py
+++ column_genre_len.py
@@ -1,1 +1,6 @@
+genre_df=movies_meta_df[["title","genre"]]
+genre_list=list(genre_df[["genre"]].values.flatten())
+unique_genre_list=list(set([item for sublist in genre_list for item in sublist]))
+
+
 genre_df['genre_len'] = genre_df.apply(lambda x: len(x['genre']), axis=1)
