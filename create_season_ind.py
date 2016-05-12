77f6b014b970449ae8536ed0271a79ff8dc762b8
charlie029 <chailin.charlie@gmail.com>
2016-05-12 16:23:43 -0400

    Create create_season_ind.py

---
+++ create_season_ind.py
@@ -0,0 +1,16 @@
+def create_season(combined_df):
+    '''
+    This function change release_date to date and create season variables based on released date.
+    '''
+    combined_df['release_date']=pd.to_datetime(combined_df['release_date'])
+    def month_to_season(x):
+        if x.month <=4:
+            return 'spring'
+        elif x.month <=7:
+            return 'summer'
+        elif x.month <=10:
+            return 'fall'
+        else: 
+            return 'christmas'
+    combined_df['release_season'] = combined_df.apply(lambda x: month_to_season(x['release_date']), axis=1)
+    return combined_df
