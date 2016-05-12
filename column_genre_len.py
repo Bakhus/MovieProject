167d476d1abecd36b44dcb95e2a7f9461915029c
ds1c1 <apple9one@gmail.com>
2016-05-12 17:13:35 -0400

    Update column_genre_len.py

--- column_genre_len.py
+++ column_genre_len.py
@@ -4,3 +4,30 @@ unique_genre_list=list(set([item for sublist in genre_list for item in sublist])
 
 
 genre_df['genre_len'] = genre_df.apply(lambda x: len(x['genre']), axis=1)
+
+
+[u'Sci-Fi',
+ u'Crime',
+ u'Romance',
+ u'Animation',
+ u'Music',
+ u'Adult',
+ u'Comedy',
+ u'War',
+ u'Horror',
+ u'Film-Noir',
+ u'Western',
+ u'News',
+ u'Thriller',
+ u'Adventure',
+ u'Mystery',
+ u'Short',
+ u'Drama',
+ u'Action',
+ u'Documentary',
+ u'Musical',
+ u'History',
+ u'Family',
+ u'Fantasy',
+ u'Sport',
+ u'Biography']
