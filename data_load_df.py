5f8a1b4128f9b9c7c40296f2897079125efc4b3e
ds1c1 <apple9one@gmail.com>
2016-05-12 13:04:34 -0400

    Update data_load_df.py
    update for all

--- data_load_df.py
+++ data_load_df.py
@@ -20,27 +20,18 @@ DATA_DIR = os.path.join('C:\\Users\\EVW207\\Desktop\\bootcamp_git\\ct16_cap1_ds5
 
 movie_files=os.listdir(DATA_DIR)
 
-wanted_keys = ['user_score',
- 'rating',
- 'complete',
- 'runtime_minutes',
- 'release_date',
- 'director',
- 'metascore',
- 'num_user_ratings',
- 'metacritic_page',
- 'studio',
- 'year',
- 'title'] # The keys you want
-
 movie_list=[]
 for names in movie_files:
     target_file_path = os.path.join(DATA_DIR, names)
     with open(target_file_path, 'r') as names:
          movie = json.load(names)
-         movie1 = dict((k, movie[k]) for k in wanted_keys if k in movie)
-    movie_list.append(movie1)
+         movie_list.append(movie)
 
 len(movie_list)
 
-movies_meta_df = pd.DataFrame(movie_list)
+list_types = []
+for i in movie_list:
+    if type(i) == dict:
+        list_types.append(i)
+        
+movies_meta_df = pd.DataFrame(list_types)
