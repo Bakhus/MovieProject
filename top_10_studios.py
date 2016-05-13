accc4bf0db411f2b4eeda72a665c463b5a7e374f
evgenymun <evgeny.mun@gmail.com>
2016-05-13 10:14:24 -0400

    Notebook and final csv for the model

--- top_10_studios.py
+++ top_10_studios.py
@@ -1,5 +1,7 @@
-'''Create column per each studio if studio is in top 10'''
 def add_studios(me_df):
+    '''Creates column per each studio if studio is in top 10. The top ten are hard coded.'''
+
+    #List of top ten studios
     top_10_studios = ['Warner Bros. Pictures'
     ,'Universal Pictures'
     ,'Columbia Pictures'
@@ -10,25 +12,25 @@ def add_studios(me_df):
     ,'IFC Films'
     ,'Miramax Films'
     ,'Fox Searchlight Pictures']
-    
+
     def in_top_10(row):
         if row['studio'] in top_10_studios:
             return 1
         else:
             return 0
-    
+
     for studio in top_10_studios:
         me_df[studio] = me_df.apply(in_top_10, axis=1)
-    
-    me_df.rename(columns={'Warner Bros. Pictures'                  : 'WarnerBrosPictures'                
-    ,'Universal Pictures'                     : 'UniversalPictures'                 
-    ,'Columbia Pictures'                      : 'ColumbiaPictures'                  
-    ,'Paramount Pictures'                     : 'ParamountPictures'                 
+
+    me_df.rename(columns={'Warner Bros. Pictures'                  : 'WarnerBrosPictures'
+    ,'Universal Pictures'                     : 'UniversalPictures'
+    ,'Columbia Pictures'                      : 'ColumbiaPictures'
+    ,'Paramount Pictures'                     : 'ParamountPictures'
     ,'Twentieth Century Fox Film Corporation' : 'TwentiethCenturyFoxFilmCorporation'
-    ,'Sony Pictures Classics'                 : 'SonyPicturesClassics'              
-    ,'Buena Vista Pictures'                   : 'BuenaVistaPictures'                
-    ,'IFC Films'                              : 'IFCFilms'                          
-    ,'Miramax Films'                          : 'MiramaxFilms'                      
+    ,'Sony Pictures Classics'                 : 'SonyPicturesClassics'
+    ,'Buena Vista Pictures'                   : 'BuenaVistaPictures'
+    ,'IFC Films'                              : 'IFCFilms'
+    ,'Miramax Films'                          : 'MiramaxFilms'
     ,'Fox Searchlight Pictures'               : 'FoxSearchlightPictures' }, inplace=True)
-    
+
     return me_df
